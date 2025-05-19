0839b11c6e91a38e2678adf93e1f3767894b13be
nicolasmenaca <nicolasandresmenacatrujillo@gmail.com>
2025-05-19 10:44:48 -0500

    Update index.ts

--- post-service/src/index.ts
+++ post-service/src/index.ts
@@ -22,8 +22,7 @@ const PORT = process.env.PORT ? Number(process.env.PORT) : 3002;
 
 AppDataSource.initialize()
     .then(() => {
-        app.use("/api", initPostRoutes(AppDataSource));
-
+        app.use("/api", initPostRoutes);
         app.listen(PORT, '0.0.0.0', () => {
             console.log(`Post service running on port ${PORT}`);
             console.log(`Swagger docs available at http://localhost:${PORT}/api/docs`);
